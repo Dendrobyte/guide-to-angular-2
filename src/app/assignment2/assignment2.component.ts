8e78ba4f00198399fe693f6170b386f60875567a
Dendrobyte <markbacon78@gmail.com>
2022-02-09 15:15:34 -0500

    Wrap up intro stuff

---
+++ src/app/assignment2/assignment2.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assignment2',
+  templateUrl: './assignment2.component.html',
+  styleUrls: ['./assignment2.component.css']
+})
+export class Assignment2Component implements OnInit {
+  outputName = "";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  setOutputName(string) {
+    this.outputName = string;
+  }
+
+  clearField() {
+    this.outputName = "";
+  }
+
+}
---
+++ src/app/assignment3/assignment3.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assignment3',
+  templateUrl: './assignment3.component.html',
+  styleUrls: ['./assignment3.component.css']
+})
+export class Assignment3Component implements OnInit {
+
+  showText: boolean = false;
+  buttonClicks: number[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  updateTextOnClick() {
+    this.showText ? this.showText = false : this.showText = true;
+    this.buttonClicks.push(this.buttonClicks.length);
+  }
+
+}
