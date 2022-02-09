87c818ab4b82c5dc8f10fae89b0a3078eebfd9a0
Dendrobyte <markbacon78@gmail.com>
2022-02-09 15:17:51 -0500

    Reset for course proj

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { WarningAlertComponent } from './assignment1/warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './assignment1/success-alert/success-alert.component';
-import { Assignment2Component } from './assignment2/assignment2.component';
 import { FormsModule } from '@angular/forms';
-import { Assignment3Component } from './assignment3/assignment3.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent,
-    Assignment2Component,
-    Assignment3Component
+    AppComponent
   ],
   imports: [
     BrowserModule,
